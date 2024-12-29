e54083fa4fc2d66804b507b0dd2251a88a55de94
Likheet Shetty <likheet.s@gmail.com>
2024-12-30 00:58:15 +0530

    Prepare for deployment

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ def send_message():
         return jsonify({"error": f"API Error: {str(e)}"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
